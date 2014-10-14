831c04cc191d8c2bc47b4feff441117e8ff0992e
Yohan Boniface <yb@enix.org>
2014-10-14 18:46:50 +0200

    Add log method to Base exporter

--- src/plugins/base-exporters/Base.js
+++ src/plugins/base-exporters/Base.js
@@ -2,4 +2,9 @@ var BaseExporter = function (project, options) {
     this.project = project;
     this.options = options;
 };
+
+BaseExporter.prototype.log = function () {
+    console.warn.apply(console, Array.prototype.concat.apply(['[Export]'], arguments));
+};
+
 exports.BaseExporter = BaseExporter;

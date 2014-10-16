f250a9c6c6b9f36214af0d1f1bcb5898377085f9
Yohan Boniface <yb@enix.org>
2014-10-16 09:23:10 +0200

    Fix mkdirs not called the proper way

--- src/back/Utils.js
+++ src/back/Utils.js
@@ -2,9 +2,10 @@ var fs = require('fs'),
     path = require('path');
 
 module.exports = {
+
     mkdirs: function (dirpath, callback) {
         fs.mkdir(dirpath, function (err) {
-            if (err && err.code === 'ENOENT') mkdirs(path.dirname(dirpath), function () {mkdirs(dirpath, callback)});
+            if (err && err.code === 'ENOENT') module.exports.mkdirs(path.dirname(dirpath), function () {module.exports.mkdirs(dirpath, callback)});
             else if (err && err.code !== 'EEXIST') callback(err);
             else callback();
         })

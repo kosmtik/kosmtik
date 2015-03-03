5ce09d040adaf5facccda3b29f4e928823754cfe
Yohan Boniface <yb@enix.org>
2015-03-03 22:49:17 +0100

    Linting

--- src/back/XRayTile.js
+++ src/back/XRayTile.js
@@ -24,24 +24,24 @@ XRayTile.prototype.styleMap = function (project) {
     var self = this,
         map = new mapnik.Map(256, 256),
         idx = 0,
-        chosenLayers = (self.options.layer)? self.options.layer.split(',') : [],
+        chosenLayers = (self.options.layer) ? self.options.layer.split(',') : [],
         layers = project.mml.Layer.reduce(function (prev, layer) {
             if (chosenLayers.length && chosenLayers.indexOf(layer.id) === -1) return prev;
             if (idx >= XRayTile.colors.length) idx = 0;
-            return prev + Utils.template(XRayTile.layer_template, {id: layer.id, rgb: XRayTile.colors[idx++]});
+            return prev + Utils.template(XRayTile.layerTemplate, {id: layer.id, rgb: XRayTile.colors[idx++]});
         }, ''),
-        xml = Utils.template(XRayTile.map_template, {layers: layers || '', bg: this.options.background || '#000000'});
+        xml = Utils.template(XRayTile.mapTemplate, {layers: layers || '', bg: this.options.background || '#000000'});
     map.fromStringSync(xml);
     return map;
 };
 
 XRayTile.prototype.stringToRGB = function (s) {
-    var hash = crypto.createHash('md5').update(s).digest('hex').slice(0,3);
+    var hash = crypto.createHash('md5').update(s).digest('hex').slice(0, 3);
     return [hash.charCodeAt(0) + 100, hash.charCodeAt(1) + 100, hash.charCodeAt(2) + 100].join(',');
 };
 
-XRayTile.map_template = fs.readFileSync(path.join(__dirname, 'xray', 'map.xml'), 'utf8');
-XRayTile.layer_template = fs.readFileSync(path.join(__dirname, 'xray', 'layer.xml'), 'utf8');
+XRayTile.mapTemplate = fs.readFileSync(path.join(__dirname, 'xray', 'map.xml'), 'utf8');
+XRayTile.layerTemplate = fs.readFileSync(path.join(__dirname, 'xray', 'layer.xml'), 'utf8');
 XRayTile.colors = [
     '218,223,225', '217,30,24', '102,51,153', '68,108,179', '247,202,24', '38,166,91', '78,205,196', '219,10,91', '232,126,4', '135,211,124'
 ];

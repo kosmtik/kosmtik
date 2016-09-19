d1d050983b9963b06b82841b59a34b382e7f218a
Jesse Crocker <jesse@gaiagps.com>
2016-09-19 09:54:07 -0600

    Set user agent to kosmtik

--- src/back/Helpers.js
+++ src/back/Helpers.js
@@ -1,4 +1,7 @@
-var request = require('request');
+var request = require('request'),
+    kosmtikPackage = require('../../package'),
+    version = kosmtikPackage.version;
+
 
 var Helpers = function (config) {
     this.config = config;
@@ -6,6 +9,8 @@ var Helpers = function (config) {
 
 Helpers.prototype.request = function (options, callback) {
     if(this.config.parsed_opts.proxy) options.proxy = this.config.parsed_opts.proxy;
+    if(!options.headers) options.headers = {};
+    options.headers["User-Agent"] = "kosmtik " + version;
     return request(options, callback);
 };
 

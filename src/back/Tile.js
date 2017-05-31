ff098bda950d5d219457429dd69ac7c4bae2d002
nebulon42 <nebulon42@mailbox.org>
2017-05-31 17:05:55 +0200

    use scale=1 as fallback if scale is not defined in the mml, fixes #241

--- src/back/Tile.js
+++ src/back/Tile.js
@@ -31,7 +31,7 @@ Tile.prototype.render = function (project, map, cb) {
     this.setupBounds();
     map.zoomToBox(this.projection.forward([this.minX, this.minY, this.maxX, this.maxY]));
     var im = new mapnik.Image(this.height, this.width);
-    map.render(im, {scale: project.mml.scale, variables: {zoom: this.z}}, cb);
+    map.render(im, {scale: project.mml.scale || 1, variables: {zoom: this.z}}, cb);
 };
 
 Tile.prototype.renderToVector = function (project, map, cb) {
